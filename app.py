94f898200db9c640ee72d269b2a7b7f2c42fbf56
marwanminecraftss-stack <marwanminecraftss@gmail.com>
2026-08-08 08:23:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = '/'
 
-
 class Base(DeclarativeBase):
   pass
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cardwarskingdom.db"
