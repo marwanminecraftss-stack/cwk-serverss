38334f402e4aba6aaf86e8e2d616944dc01b45de
marwanminecraftss-stack <marwanminecraftss@gmail.com>
2026-08-08 10:51:22 +0400

    Update app.py

--- app.py
+++ app.py
@@ -51,11 +51,14 @@ login_manager.login_view = '/'
 
 class Base(DeclarativeBase):
   pass
+import os
+
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
-db = SQLAlchemy(model_class=Base)
-db.init_app(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url or "sqlite:///cardwarskingdom.db"
+
 
 badcharaters = ['/', '\\', ':', '*', '?', '"', '<', '>', '|', ";", "%", "^", "&", "(", ")", "{", "}", "[", "]", ".", ",", "'", "`", "!", "$", "#", "@", "+", "="]
 
